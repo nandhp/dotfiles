dc397802d6281ec03c8d8de46322ed4d18d36027
nandhp <nandhp@gmail.com>
2014-12-03 15:15:23 -0600

    emacs: Have C-c C-o work in C++ model also

--- emacs/20keybindings.init.el
+++ emacs/20keybindings.init.el
@@ -74,7 +74,7 @@ point reaches the beginning or end of the buffer, stop there."
 
 ;; C-c C-o for compare-windows.
 (global-set-key (kbd "C-c C-o") 'compare-windows)
-(add-hook 'c-mode-hook     ; c-mode redefines C-c C-o for c-set-offset
+(add-hook 'c-mode-common-hook ; c-mode redefines C-c C-o for c-set-offset
           (lambda () (local-set-key (kbd "C-c C-o") 'compare-windows)))
 
 ;; <f12> to make a buffer go away (without killing it)
