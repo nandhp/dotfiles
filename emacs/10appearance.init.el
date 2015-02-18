dd847b419424011bdeed8edf42e8eedc07241627
nandhp <nandhp@gmail.com>
2015-02-17 22:28:34 -0600

    emacs: Adjust whitespace in customize comment

--- emacs/10appearance.init.el
+++ emacs/10appearance.init.el
@@ -54,10 +54,10 @@
 
 ;; Set faces
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((((type ns)) (:family "Menlo"))
 	    (t (:height 97 :family "DejaVu Sans Mono"))))
  '(hl-line ((((background light)) (:background "gray95"))
