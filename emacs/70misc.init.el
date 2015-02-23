4351658eb5823e3d7f45dba4639bd4da9fd83dda
nandhp <nandhp@gmail.com>
2015-02-22 20:13:55 -0600

    emacs: Enable uniquify in Emacs 23, savehist, and browse-kill-ring

--- emacs/70misc.init.el
+++ emacs/70misc.init.el
@@ -14,14 +14,25 @@
 ;; Automatic (de)compression of files
 (auto-compression-mode t)
 
+;; Uniquify buffer names using directory names instead of numbers
+;; (new default in Emacs 24)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(require 'uniquify nil 'noerror)
 
 ;; Start emacsclient server
 (server-start)
 
 ;;; Save minibuffer history between sessions.
-;;(setq savehist-additional-variables     ; Also save searches
-;;      '(search-ring regexp-search-ring))
-;;(savehist-mode t)
+(setq savehist-file "~/.emacs.d/history")
+(setq savehist-additional-variables
+      '(search-ring regexp-search-ring)) ; Also save searches
+(savehist-mode t)
+
+;; If browse-kill-ring is available, M-y will activate the kill-ring
+;; browser if the previous command was not a yank.
+(when (require 'browse-kill-ring nil 'noerror)
+  (browse-kill-ring-default-keybindings)
+  (setq browse-kill-ring-quit-action 'save-and-restore))
 
 ;; Set the current working directory to HOME if running on a window system.
 ;; Needed in Emacs.app, which always defaults to /.
