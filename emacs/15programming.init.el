bf0c7fc1ec9717f0d975ada4cdfcdd5fc6aedfe8
nandhp <nandhp@gmail.com>
2017-03-06 21:40:58 -0600

    emacs: Add octave-mode customizations

--- emacs/15programming.init.el
+++ emacs/15programming.init.el
@@ -16,7 +16,8 @@
 ;; prog-mode is new in Emacs 24, so we have to enumerate the modes for now.
 (setq prog-hook-list '(python-mode-hook perl-mode-hook
                        c-mode-common-hook js-mode-hook
-                       sh-mode-hook emacs-lisp-mode-hook))
+                       sh-mode-hook emacs-lisp-mode-hook
+                       octave-mode-hook))
 (defun add-prog-hook (hook)
   (mapc (lambda (mode-hook) (add-hook mode-hook hook)) prog-hook-list))
 ;; FIXME: If running Emacs 24, just use prog-mode-hook instead
@@ -38,7 +39,33 @@
     (setq comment-end (concat " " comment-end))))
 (add-hook 'css-mode-hook 'patch-css-mode-comment-syntax)
 
-;; Make MATLAB mode less annoying
+;; Octave mode
+;; See also http://ubuntuforums.org/showthread.php?t=1566508&page=3
+(setq octave-block-offset 4)
+(setq octave-continuation-string "...")
+(setq octave-block-comment-start "% ")  ; Use %, not #, for comments
+(setq octave-comment-start "% ")
+(setq octave-comment-char 37)
+
+;; Set comment-add to 0 to avoid Elisp-style distinguishing between %
+;; and %%.
+(defun my-octave-mode ()
+  (setq-local comment-add 0))
+(add-hook 'octave-mode-hook 'my-octave-mode)
+
+;; Sometimes that is not enough, so advise octave-indent-comment to
+;; always return nil.
+(defadvice octave-indent-comment (around octave-dont-indent-commment ())
+  "Don't indent comments."
+  nil) ; (if (> ad-return-value 0) (setq ad-return-value 0)))
+(ad-activate 'octave-indent-comment)
+
+;; Use octave-mode for .m files, not MATLAB mode (if installed) or
+;; Objective-C mode
+(if (fboundp 'octave-mode)
+    (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode)))
+
+;; Also make MATLAB mode less annoying, just in case
 (setq matlab-auto-fill nil)
 
 ;; Use Markdown mode for Markdown files
