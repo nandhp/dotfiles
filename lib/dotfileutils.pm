cf4dec45ccfd6cd0db741d74072605ced0533988
nandhp <nandhp@gmail.com>
2017-07-13 19:50:59 -0500

    Minor improvements to run-parts, rename ssh_config

--- lib/dotfileutils.pm
+++ lib/dotfileutils.pm
@@ -18,7 +18,7 @@ use strict;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(run_parts $HOSTNAME);
+our @EXPORT = qw(run_parts $HOSTNAME $HOME);
 
 # For relative pathnames
 my @caller = caller();
@@ -32,17 +32,20 @@ find({wanted => sub { push @PATH, $_ if -d $_ and $_ !~ /\/\./ },
 # Include all files matching $glob in the include path.
 # For example, run_parts('*.bashrc');
 sub run_parts {
-    my ($glob, $commentchar) = @_;
+    my ($glob, $commentchar, $func) = @_;
+    $func ||= \&include;
     printf("\n%s (begin run_parts %s)\n", $commentchar, $glob) if $commentchar;
     foreach my $file ( sort basename_sort path_glob $glob ) {
+        next if $file =~ /\~$/; # Editor backup files
         printf("\n%s %s\n\n", $commentchar,
                File::Spec->abs2rel($file, dirname($origfile))) if $commentchar;
-        include $file;
+        $func->($file);
     }
     printf("\n%s (end run_parts %s)\n", $commentchar, $glob) if $commentchar;
 }
 
 # Some useful variables
 (our $HOSTNAME = hostname) =~ s/\..*//;
+our $HOME = $ENV{HOME};
 
 1;
